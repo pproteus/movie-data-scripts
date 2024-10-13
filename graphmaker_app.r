351788de39f62c1ba50da68d29ddbc265f1d2d3f
unknown <paulproteus47@gmail.com>
2024-10-13 13:46:40 -0400

    Added genre/content filters to shiny app

--- graphmaker_app.r
+++ graphmaker_app.r
@@ -16,7 +16,9 @@ ui = page_sidebar(
 		checkboxInput("yeslabels", "Show labels"),
 		sliderInput("size", "Size", min=0.5, max=3, value=1.8),
 		sliderInput("xrange", "X Range", min=0, max=100, value=c(0, 100), round=1),
-		sliderInput("yrange", "Y Range", min=0, max=100, value=c(0, 100), round=1)
+		sliderInput("yrange", "Y Range", min=0, max=100, value=c(0, 100), round=1),
+		textInput("genrefilter", "Exclude Genres", value="Crime|Horror|Western|Documentary"),
+		textInput("contentfilter", "Exclude Content", value="Intense violence|Abundant Swearing|Abundant Sexual Content")
 		),
 	plotOutput("plot", hover=hoverOpts(id="plot_hover", delayType = c("throttle")))
 )
@@ -55,24 +57,61 @@ server = function(input, output){
 		req(plot_info$xvar)
 		plot_info$x = plot_info$df[[plot_info$xvar]]
 		plot_info$y = plot_info$df[[plot_info$yvar]]
-		xrange = range(plot_info$x, na.rm=T)
-		yrange = range(plot_info$y, na.rm=T)
+		xrange = round(range(plot_info$x, na.rm=T), 1)
+		yrange = round(range(plot_info$y, na.rm=T), 1)
+		req(xrange)
 		updateSliderInput(inputId="xrange", min=xrange[1], max=xrange[2], 
 			value=xrange, step=diff(xrange)/100)
 		updateSliderInput(inputId="yrange", min=yrange[1], max=yrange[2], 
 			value=yrange, step=diff(yrange)/100)
 	}
 
+	apply_filters = function(){
+		req(plot_info$df)
+		if (input$contentfilter != ""){
+			tryCatch(
+				{plot_info$content_flag = grepl(input$contentfilter, plot_info$df$Objectionable.Content, ignore.case=T, perl=T) }
+				, error = function(e) plot_info$content_flag = rep(F, length(plot_info$x))
+				, warning = function(e) plot_info$content_flag = rep(F, length(plot_info$x))
+			)
+		}
+		else {
+			plot_info$content_flag = rep(F, length(plot_info$x))
+		}
+		if (input$genrefilter != ""){
+			tryCatch(
+				{plot_info$genre_flag = grepl(input$genrefilter, plot_info$df$Genres, ignore.case=T, perl=T) }
+				, error = function(e) plot_info$genre_flag = rep(F, length(plot_info$x))
+				, warning = function(e) plot_info$genre_flag = rep(F, length(plot_info$x))
+			)
+		}
+		else {
+			plot_info$genre_flag = rep(F, length(plot_info$x))
+		}
+		plot_info$filter_flag = plot_info$content_flag | plot_info$genre_flag
+	}
+
 	observeEvent(input$filename, {
 		df = read.csv(input$filename$datapath, sep="\t")
 		plot_info$df = df
-		graph_setup(input$graphtype)
-		plot_info$labels = df$Title
+		plot_info$df[["Letterboxd.Rating"]] = jitter(plot_info$df[["Letterboxd.Rating"]])
+		plot_info$df[["IMDB.Rating"]] = jitter(plot_info$df[["IMDB.Rating"]])
+		apply_filters()
 		plot_info$highlight_flag = rep(F, length(plot_info$x))
+		graph_setup(input$graphtype)
+		plot_info$labels = paste(plot_info$df$Title, " (", plot_info$df$Year, ")", sep="")
 	})
 
 	observeEvent(input$graphtype, {
 		graph_setup(input$graphtype)
+	})
+
+	observeEvent(input$contentfilter, {
+		apply_filters()
+	})
+
+	observeEvent(input$genrefilter, {
+		apply_filters()
 	})
 
 	observeEvent(input$plot_hover, {
@@ -87,7 +126,8 @@ server = function(input, output){
 		req(plot_info$x, plot_info$y)
 		plot(plot_info$y~plot_info$x, pch=19, cex=input$size, cex.lab=sqrt(input$size), 
 				xlab=plot_info$xlab, ylab=plot_info$ylab, log=plot_info$log, 
-				xlim=input$xrange, ylim=input$yrange)
+				xlim=input$xrange, ylim=input$yrange, col=ifelse(plot_info$filter_flag, "grey", "black"))
+
 		if (input$graphtype == RATING_GRAPH_STRING){
 			abline(0,2, lty=2)
 			fit = lm(y~x+I(x^2), data=data.frame(x=plot_info$x, y=plot_info$y))
@@ -103,7 +143,11 @@ server = function(input, output){
 			abline(v=1, lty=2)
 		}
 		if (input$yeslabels) {
-			text(plot_info$x, plot_info$y, offset=0.5, pos=3, plot_info$labels, cex=0.7*input$size)
+			text(plot_info$x, plot_info$y, offset=0.5, pos=3, plot_info$labels, cex=0.7*input$size, col="grey")
+			if (! all(plot_info$filter_flag)) {
+				text(plot_info$x[which(!plot_info$filter_flag)], plot_info$y[which(!plot_info$filter_flag)], 
+					offset=0.5, pos=3, plot_info$labels[which(!plot_info$filter_flag)], cex=0.7*input$size, col="black")
+			}
 		}
 		flag=plot_info$highlight_flag
 		if(sum(flag) & input$yeslabels)	{		
